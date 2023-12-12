d96b1fef49376f6868f78bb0d931aea156ab5d19
Carlos Roberto Gasparini <crgasparini32@gmail.com>
2023-12-12 19:37:00 -0300

    feat: preguntas desplegables

---
+++ js/app.js
@@ -0,0 +1,20 @@
+const questions = document.querySelectorAll(".question");
+
+// console.log(questions);
+
+questions.forEach(question => {
+    const btn = question.querySelector(".question__btn");    
+
+    btn.addEventListener("click", () => {
+        questions.forEach(item => {
+            if(item !== question) {
+                item.classList.remove("show-text");
+            }
+        })
+
+        question.classList.toggle("show-text");
+    })
+})
+
+
+
